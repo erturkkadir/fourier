28cc39a164083d1d081834fcf48185d8451150f0
erturkkadir <kadirerturk@gmail.com>
2020-11-29 19:26:16 -0800

    Credentials

--- app/src/main/java/com/ekoja/transform/MainActivity.java
+++ app/src/main/java/com/ekoja/transform/MainActivity.java
@@ -9,16 +9,17 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
